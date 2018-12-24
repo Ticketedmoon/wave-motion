3736ae79f61983e1e44c84b736a251aac9c9db44
Shane Creedon <Shane.creedon3@mail.dcu.ie>
2018-12-24 17:18:13 +0000

    Add Runnable Jar

--- Wave-Motion/src/main/java/WaveMotion.java
+++ Wave-Motion/src/main/java/WaveMotion.java
@@ -11,7 +11,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.BufferedInputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
